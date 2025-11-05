6f2b39aa540233b8030e7deaded0aa428eaf2147
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-11-05 13:43:48 +0200

    Added Robots & Sitemap.xml

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+import { getAllBlogPosts } from "@/lib/blog";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://bettercallmo.com";
+  const posts = await getAllBlogPosts();
+
+  const blogPosts = posts.map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: post.frontmatter.updated || post.frontmatter.date,
+    changeFrequency: "monthly" as const,
+    priority: 0.7,
+  }));
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...blogPosts,
+  ];
+}
