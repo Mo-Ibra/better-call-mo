419a20b604d7829d311f022cc9affc3a491c8b79
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-11-07 07:46:52 +0200

    Fix Issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,9 +49,10 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
+      // { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
       { url: "/favicon.svg", type: "image/svg+xml" },
     ],
     apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
